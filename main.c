258a0fcf62131cea139301fff8964f8b4f5e5588
L4ze3 <73757315+L4ze3@users.noreply.github.com>
2024-06-12 23:09:57 +0200

    Changed Stuff, added player_count, dealer_count
    TODO: file handling, betting

--- main.c
+++ main.c
@@ -10,28 +10,8 @@
 enum Suits { HEARTS, CLUBS, DIAMONDS, SPADES };
 enum Ranks { ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING };
 
-const char Suit[4][5] = {
-    { "♥" },
-    { "♣" },
-    { "♦" },
-    { "♠" },
-};
-
-const char Rank[13][5] = {
-    { "A" },
-    { "2" },
-    { "3" },
-    { "4" },
-    { "5" },
-    { "6" },
-    { "7" },
-    { "8" },
-    { "9" },
-    { "10" },
-    { "J" },
-    { "Q" },
-    { "K" },
-};
+const char *Suit[] = { "♥", "♣", "♦", "♠" };
+const char *Rank[] = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
 
 struct Card {
     char suit[10];
@@ -45,21 +25,20 @@ struct Card dealer_hand[MAX_CARDS];
 /* Function prototypes */
 void initialize_deck(struct Card *deck);
 void shuffle(struct Card *deck, int size);
-void deal_card(struct Card *hand, struct Card *deck, int *track);
-void print_hand(struct Card *hand, int track);
+void deal_card(struct Card *hand, int *hand_count, struct Card *deck, int *deck_position);
+void print_hand(struct Card *hand, int hand_count, bool hide_first);
 int menu();
-int eval(struct Card *hand, int track);
-int check_win(struct Card *player, struct Card *dealer, int track);
+int eval(struct Card *hand, int hand_count);
+bool check_win(struct Card *player, int player_count, struct Card *dealer, int dealer_count);
 
 void initialize_deck(struct Card *deck)
 {
     int index = 0;
-
     for (enum Suits suit = HEARTS; suit <= SPADES; suit++) {
         for (enum Ranks rank = ACE; rank <= KING; rank++) {
-             strcpy(deck[index].rank, Rank[rank]);
-             strcpy(deck[index].suit, Suit[suit]);
-             index++;
+            strcpy(deck[index].rank, Rank[rank]);
+            strcpy(deck[index].suit, Suit[suit]);
+            index++;
         }
     }
 }
@@ -75,20 +54,26 @@ void shuffle(struct Card *deck, int size)
     }
 }
 
-void deal_card(struct Card *hand, struct Card *deck, int *track)
+void deal_card(struct Card *hand, int *hand_count, struct Card *deck, int *deck_position)
 {
-    hand[*track] = deck[*track];
-    //if (hand == player_hand)
-        *track = *track + 1;
+    hand[*hand_count] = deck[*deck_position];
+    (*hand_count)++;
+    (*deck_position)++;
 }
 
-void print_hand(struct Card *hand, int track)
+void print_hand(struct Card *hand, int hand_count, bool hide_first)
 {
-    for (int i = 0; i < track; i++) {
-        //printf("%s:\n", (hand == player_hand) ? "Your Hand" : "Dealers Hand" );
-        printf("%s%s ", hand[i].rank, hand[i].suit);
+    for (int i = 0; i < hand_count; i++) {
+        if (hide_first && i == 0) {
+            printf("?? ");
+        } else {
+            printf("%s%s ", hand[i].rank, hand[i].suit);
+        }
     }
-    printf("\nValue: %d\n", eval(hand, track));
+    if (!hide_first) {
+        printf("\nValue: %d", eval(hand, hand_count));
+    }
+    printf("\n");
 }
 
 int menu()
@@ -97,17 +82,17 @@ int menu()
     printf("Choose an action:\n");
     printf("Hit\t(1)\n");
     printf("Stand\t(2)\n");
-    printf("\033[35m> \033[0m");
+    printf("> ");
     scanf("%d", &action);
     return action;
 }
 
-int eval(struct Card *hand, int track)
+int eval(struct Card *hand, int hand_count)
 {
     int sum = 0;
     int num_aces = 0;
 
-    for (int i = 0; i < track; i++) {
+    for (int i = 0; i < hand_count; i++) {
         if (strcmp(hand[i].rank, "A") == 0) {
             num_aces++;
         } else if (strcmp(hand[i].rank, "J") == 0 || strcmp(hand[i].rank, "Q") == 0 || strcmp(hand[i].rank, "K") == 0) {
@@ -125,64 +110,86 @@ int eval(struct Card *hand, int track)
         }
         num_aces--;
     }
-    
+
     return sum;
 }
 
-int check_win(struct Card *player, struct Card *dealer, int track)
+bool check_win(struct Card *player, int player_count, struct Card *dealer, int dealer_count)
 {
-    int win;
-    if (eval(player, track) > 21)
-        win = 0;
-    else if (eval(dealer, track) > 21)
-        win = 1;
-    else if (eval(player, track) > eval(dealer, track))
-        win = 0;
-    else
-        win = 1;
+    int player_value = eval(player, player_count);
+    int dealer_value = eval(dealer, dealer_count);
 
-    return win;
+    if (player_value > 21) {
+        printf("Player busts!\n");
+        return false;
+    } else if (dealer_value > 21) {
+        printf("Dealer busts! Player wins!\n");
+        return true;
+    } else if (player_value > dealer_value) {
+        printf("Player wins!\n");
+        return true;
+    } else {
+        printf("Dealer wins!\n");
+        return false;
+    }
 }
 
 int main(void)
 {
     srand(time(NULL));
 
-    int bet, action;
-    int track = 0; /* Tracks the amount of dealt cards */
+    int bet;
+    int player_count = 0;
+    int dealer_count = 0;
+    int deck_position = 0;
     bool gameloop = true;
+    bool player_turn = true;
 
     system("clear");
 
     initialize_deck(deck);
     shuffle(deck, DECK_SIZE);
 
-    deal_card(player_hand, deck, &track);
-    deal_card(dealer_hand, deck, &track);    
-    deal_card(player_hand, deck, &track);
+    deal_card(player_hand, &player_count, deck, &deck_position);
+    deal_card(dealer_hand, &dealer_count, deck, &deck_position);
+    deal_card(player_hand, &player_count, deck, &deck_position);
+    deal_card(dealer_hand, &dealer_count, deck, &deck_position);
 
     while (gameloop) {
+        system("clear");
 
-        print_hand(player_hand, track);
-        printf("\n\n");
-        print_hand(dealer_hand, track);
+        printf("Your Hand:\n");
+        print_hand(player_hand, player_count, false);
+        printf("\nDealer's Hand:\n");
+        print_hand(dealer_hand, dealer_count, player_turn);
 
-        action = menu();
+        if (player_turn) {
+            int action = menu();
 
-        if (action == 1) {
-            deal_card(player_hand, deck, &track);
-        } else if (action == 2) {
-            while (eval(dealer_hand, track) <= 17)
-                deal_card(dealer_hand, deck, &track);
-        }
-    
-        if (check_win(player_hand, dealer_hand, track) == 1) {
-            printf("Loss!\n");
+            if (action == 1) {
+                deal_card(player_hand, &player_count, deck, &deck_position);
+                if (eval(player_hand, player_count) > 21) {
+                    player_turn = false;
+                }
+            } else if (action == 2) {
+                player_turn = false;
+            }
         } else {
-            printf("Win!\n"); 
+            while (eval(dealer_hand, dealer_count) <= 17) {
+                deal_card(dealer_hand, &dealer_count, deck, &deck_position);
+            }
+            gameloop = false;
         }
-        
     }
-        
+    
+    printf("\nFinal Hands:\n");
+    printf("Your Hand:\n");
+    print_hand(player_hand, player_count, false);
+    printf("\nDealer's Hand:\n");
+    print_hand(dealer_hand, dealer_count, false);
+
+    check_win(player_hand, player_count, dealer_hand, dealer_count);
+
     return 0;
 }
+
